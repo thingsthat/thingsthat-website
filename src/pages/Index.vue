12182b3fc5d21413632bf49fcf0de4717d80ebc3
Jack <jack@thingsthat.com>
2021-02-06 20:21:00 +0000

    Short portfolio video. Needs a bit more work

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -8,7 +8,16 @@
                     
                     <p>I'm a big fan of building products that are intuitive and fun, eliminating noise through good code and simple design. Specialising in content management, publishing, e-commerce, live streaming and real-time applications, video and audio, and storytelling.</p>
 
-                    <p>I've worked with companies such as Air New Zealand, Tesco, News UK, IBM, and The Economist. Built and lead teams at agencies. And created websites for <a href="https://www.beryl.cc" target="_blank" class="external">Beryl</a> and <a href="https://www.bullboat.co.uk" target="_blank" class="external">Bullboat</a>. Alongside my freelance work, I also design and build my own products such as <a href="https://www.unalike.net" target="_blank" class="external">Unalike</a>.</p>
+                    <p>I've worked with companies such as Air New Zealand, Tesco, News UK, IBM, and The Economist. Built and lead teams at agencies. And created websites for <a href="https://www.beryl.cc" target="_blank" class="external">Beryl</a> and <a href="https://www.bullboat.co.uk" target="_blank" class="external">Bullboat</a>. Alongside my client work, I also design and build my own products such as <a href="https://www.unalike.net" target="_blank" class="external">Unalike</a>.</p>
+                </div>
+            </section>
+
+            <section id="home__portfolio">
+                <div class="inner">
+                    <video autoplay muted loop playsinline>
+                        <source src="/portfolio.mp4" type="video/mp4">
+                        <source src="/portfolio.webm" type="video/webm">
+                    </video>
                 </div>
             </section>
             
